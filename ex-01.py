3a04bf66b92d21e7e9ec43070ab8ff7a11fa5500
RafaelSilvaz <nagatoshinjizin@gmail.com>
2025-03-13 19:54:51 -0300

    Create ex-01.py

---
+++ ex-01.py
@@ -0,0 +1,5 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+print("Rafael Agostinho")
+print('Rio de Janeiro')
+print('Rio de Janeiro')
