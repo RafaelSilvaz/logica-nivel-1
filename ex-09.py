8406d56e68d30154c8005eb0a922e1871c81bab7
RafaelSilvaz <nagatoshinjizin@gmail.com>
2025-03-20 19:15:45 -0300

    Create ex-09.py

---
+++ ex-09.py
@@ -0,0 +1,6 @@
+#9)Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+if(12>10):
+    print("é maior que 10")
+else:
+    print("é menor que 10")
+    
